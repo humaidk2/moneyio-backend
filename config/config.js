7f41fa8e4f5cdc3c4d61228413973e3f0030d342
Humaid Khan <humaidk2@gmail.com>
2020-07-06 21:07:40 +0400

    create config file to app configuraation

---
+++ config/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'secret': '',
+    'dbUser': '',
+    'dbPassword': '',
+    'dbHost': '',
+    'FACEBOOK_APP_ID': '',
+    'FACEBOOK_APP_SECRET': '',
+    // link switch between http://vrpacman.com and http://localhost:3000
+    'link': ''
+  
+  }
+  
