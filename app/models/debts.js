70664efd1e909f4791a10716977d8ba1b9170f0c
Humaid Khan <humaidk2@gmail.com>
2020-07-06 21:05:27 +0400

    create table for debts owed storing them in mysql

---
+++ app/models/debts.js
@@ -0,0 +1,46 @@
+var Sequelize = require('sequelize');
+var supersecret = require('../../config/config');
+var sequelize = new Sequelize({
+    database:'MoneyIo', 
+    username: supersecret.dbUser, 
+    password: supersecret.dbPassword,
+    host: supersecret.dbHost,
+    dialect: 'mysql'
+  }); 
+
+var Debts = sequelize.define('Debt', {
+    type: {
+        type: Sequelize.STRING,
+        validate: {
+            len: [1, 255]
+        }
+    },
+    personOwed: {
+        type: Sequelize.INTEGER,
+        references: 'User',
+        referencesKey: 'id'
+    },
+    amount: {
+        type: Sequelize.FLOAT,
+    },
+    date: {
+        type: Sequelize.DATE,
+    },
+    user_id: {
+        type: Sequelize.INTEGER,
+        references: 'User',
+        referencesKey: 'id'
+    }
+}, {
+    freezeTableName: true
+})
+
+// // force: true will drop the table if it already exists
+// User.sync({force: true}).then(function () {
+//     return User.create({
+//         username: 'admin',
+//         email: 'testtest@gmail.com',
+//         password: 'admin'
+//     });
+// });
+module.exports = Debts
