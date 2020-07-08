6168bf2304509bbc40781aaf3ac73757204795ac
Humaid Khan <humaidk2@gmail.com>
2020-07-08 17:05:06 +0400

    remove dbName for added security

--- config/config.js
+++ config/config.js
@@ -3,6 +3,7 @@ module.exports = {
     'dbUser': '',
     'dbPassword': '',
     'dbHost': '',
+    'dbName': '',
     'FACEBOOK_APP_ID': '',
     'FACEBOOK_APP_SECRET': '',
     // link switch between http://vrpacman.com and http://localhost:3000
