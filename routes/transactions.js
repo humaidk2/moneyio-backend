f3ec615291d51ba19d64a35357b5df15132f800c
Humaid Khan <humaidk2@gmail.com>
2020-07-14 16:31:41 +0400

    Add date field to transaction insertions

--- routes/transactions.js
+++ routes/transactions.js
@@ -3,12 +3,14 @@ module.exports = function(app, isLoggedIn, Transaction) {
         var category = req.body.category;
         var title = req.body.title;
         var amount = req.body.amount;
+        var currDate = req.body.date;
         // var userID = req.session.user.id;
-        console.log('id', req.session.user.id);
+        console.log('id', req.session.passport.user.id);
         Transaction.create({
             category: category,
             title: title,
             amount: amount,
+            date: currDate,
             user_id: req.session.passport.user.id
         })
         .then(function() {
