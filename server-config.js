964a9d03a5e4c7e99872fec522a5f9de10edde24
Humaid Khan <humaidk2@gmail.com>
2020-07-14 16:32:39 +0400

    Move credentials from config file to environment variables Add relation, User has many transactionss Add relation User has many debts Add user_id foreign key to Transaction for enforcing relation Add user_id foreign key to Debt for enforcing relation Add personOwed foreign key to Debt for enforcing relation Sync/Create all tables(user, transaction, debt) and insert records Add verifyEmail Route

--- server-config.js
+++ server-config.js
@@ -3,7 +3,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 var isLoggedIn = require('./routes/isLoggedIn');
-var supersecret = require('./config/config');
 
 
 
@@ -38,7 +37,57 @@ sequelize
     console.log('Unable to connect to the database:', err);
   });
 
-require('./config/passport')(passport); //pass passport for configuration
+let User = require('./app/models/User');
+var Transaction = require('./app/models/Transaction');
+var Debt = require('./app/models/Debt');
+User.hasMany(Transaction);
+Transaction.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+User.hasMany(Debt);
+Debt.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+Debt.belongsTo(User, {
+  foreignKey: 'personOwed'
+});
+User.sync({force: false}).then(function () {
+  return;
+  User.create({
+    username: 'admin2',
+    email: 'test2test@gmail.com',
+    password: 'admin2'
+  });
+  return User.create({
+    username: 'admin',
+    email: 'testtest@gmail.com',
+    password: 'admin'
+  });
+});
+Transaction.sync({force: true}).then(function () {
+    return;
+    // return Transaction.create({
+    //   category: "car",
+    //   title: "car repairing",
+    //   amount: 53.4,
+    //   date:  new Date(Date.UTC(2016, 0, 1)),
+    //   user_id: 1
+    // });
+  }).catch((error)=>{
+    console.log("error with transaction");
+    console.log(error);
+});
+Debt.sync({force: true}).then(function () {
+    return;
+    // return Debt.create({
+    //   type: 'entertainment',
+    //   personOwed: 1,
+    //   amount: 50,
+    //   date:  new Date(Date.UTC(2016, 0, 1)),
+    //   user_id: 2
+    // });
+  });
+require('./config/passport')(passport, User); //pass passport for configuration
 
 
 app.use(cookieParser());
@@ -52,8 +101,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-var Transaction = require('./app/models/Transaction');
-var Debt = require('./app/models/Debt');
+
 require('./routes/currency')(app, isLoggedIn);
 require('./routes/debts')(app, isLoggedIn, Debt);
 require('./routes/getDebts')(app, isLoggedIn, Debt);
@@ -62,7 +110,7 @@ require('./routes/logout')(app);
 require('./routes/signin')(app, passport);
 require('./routes/signup')(app, passport);
 require('./routes/transactions')(app, isLoggedIn, Transaction);
-
+require('./routes/verifyEmail')(app, User);
 
 
 module.exports = app;
