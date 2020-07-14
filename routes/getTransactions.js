ddce10b63e952c4933585681f88a3179d85ae6e5
Humaid Khan <humaidk2@gmail.com>
2020-07-14 16:24:17 +0400

    fix log to use passport to get user id, add error for missing transactions

--- routes/getTransactions.js
+++ routes/getTransactions.js
@@ -3,10 +3,14 @@ module.exports = function(app, isLoggedIn, Transaction) {
     app.get('/transactions', isLoggedIn, function(req, res) {
         var param = {};
         console.log("ok");
-        console.log('id', req.session.user.id);
+
+        console.log('id', req.session.passport.user.id);
         Transaction.findAll({
             where: {user_id: req.session.passport.user.id}
         }).then(function(transactions) {
+            if(!transactions) {
+                res.status(404).send({'Message': "No transactions here"});
+            }
             res.send(transactions);
         })
     })
