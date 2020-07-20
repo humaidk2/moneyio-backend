4e3dabf5139ff646c33aa20a427ec514a384b6b2
Humaid Khan <humaidk2@gmail.com>
2020-07-20 20:00:22 +0400

    Remove logs, Prettify for cleanliness

--- routes/getDebts.js
+++ routes/getDebts.js
@@ -1,11 +1,9 @@
-module.exports = function(app, isLoggedIn, Debt) {
-    app.get('/debts', isLoggedIn, function(req, res) {
-        Debt.findAll({
-            where: {user_id: req.session.passport.user.id}
-        }). then(function(debts){
-            res.send(debts);
-            res.status(200).send({"Message": true, debts: debts});
-        });
-    })
-}
-
+module.exports = function (app, isLoggedIn, Debt) {
+  app.get("/debts", isLoggedIn, function (req, res) {
+    Debt.findAll({
+      where: { user_id: req.session.passport.user.id },
+    }).then(function (debts) {
+      res.status(200).send({ Message: true, debts: debts });
+    });
+  });
+};
