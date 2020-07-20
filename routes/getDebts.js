943d042c3550fee0da3da73908b38a01e4abf2cf
Humaid Khan <humaidk2@gmail.com>
2020-07-20 17:02:18 +0400

    Remove unused variables, remove logs Add status, add message field, return debts where found

--- routes/getDebts.js
+++ routes/getDebts.js
@@ -1,11 +1,10 @@
 module.exports = function(app, isLoggedIn, Debt) {
     app.get('/debts', isLoggedIn, function(req, res) {
-        var param = {};
-        console.log('id', req.session.user.id);
         Debt.findAll({
             where: {user_id: req.session.passport.user.id}
         }). then(function(debts){
             res.send(debts);
+            res.status(200).send({"Message": true, debts: debts});
         });
     })
 }
