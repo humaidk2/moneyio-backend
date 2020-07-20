e5f86747e3d6f5c894001c900f0f9421afd4c40e
Humaid Khan <humaidk2@gmail.com>
2020-07-20 20:11:03 +0400

    Create new deleteDebts route that deletes debts based on user id and debt id

---
+++ routes/deleteDebts.js
@@ -0,0 +1,9 @@
+module.exports = function (app, isLoggedIn, Debt) {
+  app.post("/deleteDebts", isLoggedIn, function (req, res) {
+    Debt.destroy({
+      where: { user_id: req.session.passport.user.id, id: req.body.id },
+    }).then(function (debts) {
+      res.status(200).send({ Message: true, debts: debts });
+    });
+  });
+};
