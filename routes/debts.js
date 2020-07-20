1928c6f28a5f963da15efefb8bb793dd0c2359f5
Humaid Khan <humaidk2@gmail.com>
2020-07-20 19:47:33 +0400

    Add description field for adding debt Prettify code for cleanliness

--- routes/debts.js
+++ routes/debts.js
@@ -1,16 +1,18 @@
-module.exports = function(app, isLoggedIn, Debt) {
-  app.post('/debts',isLoggedIn, function(req, res) {
+module.exports = function (app, isLoggedIn, Debt) {
+  app.post("/debts", isLoggedIn, function (req, res) {
     var type = req.body.type;
     var person = req.body.person;
     var amount = req.body.amount;
+    var description = req.body.description;
+    console.log("cmon");
     Debt.create({
-      type: type, 
-      amount: amount, 
-      person: person, 
-      user_id: req.session.passport.user.id
-    })
-    .then(function() {
+      type: type,
+      amount: amount,
+      person: person,
+      description: description,
+      user_id: req.session.passport.user.id,
+    }).then(function () {
       res.end();
     });
-  })
-}
+  });
+};
