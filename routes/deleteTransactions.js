1b23def68d8d29ee856c87b0039a4fe11268b299
Humaid Khan <humaidk2@gmail.com>
2020-07-28 19:34:26 +0400

    return id on successful transaction deletion

--- routes/deleteTransactions.js
+++ routes/deleteTransactions.js
@@ -2,8 +2,8 @@ module.exports = function (app, isLoggedIn, Transaction) {
   app.post("/deleteTransactions", isLoggedIn, function (req, res) {
     Transaction.destroy({
       where: { user_id: req.session.passport.user.id, id: req.body.id },
-    }).then(function (transaction) {
-      res.status(200).send({ Message: true, transaction: transactio });
+    }).then(function () {
+      res.status(200).send({ Message: true, id: req.body.id });
     });
   });
 };
