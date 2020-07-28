5160101494c7d5d595d81273638350bc9d97ceb0
Humaid Khan <humaidk2@gmail.com>
2020-07-28 19:35:52 +0400

    Fix indentation, return transaction on successful insertion

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,20 +1,19 @@
-module.exports = function(app, isLoggedIn, Transaction) {
-    app.post('/transactions', isLoggedIn, function(req, res) {
-        var category = req.body.category;
-        var title = req.body.title;
-        var amount = req.body.amount;
-        var currDate = req.body.date;
-        // var userID = req.session.user.id;
-        console.log('id', req.session.passport.user.id);
-        Transaction.create({
-            category: category,
-            title: title,
-            amount: amount,
-            date: currDate,
-            user_id: req.session.passport.user.id
-        })
-        .then(function() {
-          res.end();
-        });
-    })
-}
+module.exports = function (app, isLoggedIn, Transaction) {
+  app.post("/transactions", isLoggedIn, function (req, res) {
+    var category = req.body.category;
+    var title = req.body.title;
+    var amount = req.body.amount;
+    var currDate = req.body.date;
+    // var userID = req.session.user.id;
+    console.log("id", req.session.passport.user.id);
+    Transaction.create({
+      category: category,
+      title: title,
+      amount: amount,
+      date: currDate,
+      user_id: req.session.passport.user.id,
+    }).then(function (result) {
+      res.send(result);
+    });
+  });
+};
