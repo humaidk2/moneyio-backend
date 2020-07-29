19c41d4a1069623397055a189d927cb045ce382c
Humaid Khan <humaidk2@gmail.com>
2020-07-29 18:13:46 +0400

    Remove logs for cleanup

--- routes/getTransactions.js
+++ routes/getTransactions.js
@@ -1,11 +1,9 @@
-
-module.exports = function(app, isLoggedIn, Transaction) {
-    app.get('/transactions', isLoggedIn, function(req, res) {
-        Transaction.findAll({
-            where: {user_id: req.session.passport.user.id}
-        }).then(function(transactions) {
-            console.log("hello");
-            res.status(200).send({"Message": true, transactions: transactions});
-        })
-    })
-}
+module.exports = function (app, isLoggedIn, Transaction) {
+  app.get("/transactions", isLoggedIn, function (req, res) {
+    Transaction.findAll({
+      where: { user_id: req.session.passport.user.id },
+    }).then(function (transactions) {
+      res.status(200).send({ Message: true, transactions: transactions });
+    });
+  });
+};
