816cbd9668c35ac3bff39481dca7cfb030a5d047
Humaid Khan <humaidk2@gmail.com>
2020-08-04 00:52:53 +0400

    remove catch from table definition

--- app/models/Debt.js
+++ app/models/Debt.js
@@ -8,32 +8,28 @@ var sequelize = new Sequelize({
   host: process.env.DB_HOST,
   dialect: "mysql",
 });
-var Debt = sequelize
-  .define(
-    "Debt",
-    {
-      person: {
-        type: Sequelize.STRING,
-      },
-      description: {
-        type: Sequelize.STRING,
-      },
-      type: {
-        type: Sequelize.STRING,
-      },
-      amount: {
-        type: Sequelize.FLOAT,
-      },
-      date: {
-        type: Sequelize.DATE,
-      },
+var Debt = sequelize.define(
+  "Debt",
+  {
+    person: {
+      type: Sequelize.STRING,
     },
-    {
-      freezeTableName: true,
-    }
-  )
-  .catch((error) => {
-    console.log(error);
-  });
+    description: {
+      type: Sequelize.STRING,
+    },
+    type: {
+      type: Sequelize.STRING,
+    },
+    amount: {
+      type: Sequelize.FLOAT,
+    },
+    date: {
+      type: Sequelize.DATE,
+    },
+  },
+  {
+    freezeTableName: true,
+  }
+);
 
 module.exports = Debt;
