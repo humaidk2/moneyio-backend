d137bc653e9e98e902553a470adf741df3d42c50
Humaid Khan <humaidk2@gmail.com>
2020-08-04 00:26:00 +0400

    change gmail auth to use oauth2

--- routes/signup.js
+++ routes/signup.js
@@ -5,9 +5,17 @@ var transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: false,
+  // auth: {
+  //   user: process.env.NODE_MAIL_USER,
+  //   pass: process.env.NODE_MAIL_PASS,
+  // },
   auth: {
-    user: process.env.NODE_MAIL_USER,
-    pass: process.env.NODE_MAIL_PASS,
+    type: "OAuth2",
+    user: process.env.MAIL_USER,
+    clientId: process.env.OAUTH_CLIENT_ID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    accessToken: process.env.OAUTH_ACCESS_TOKEN,
   },
 });
 
