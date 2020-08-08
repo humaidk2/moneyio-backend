6cd7b4d77dc9767968750245fae30e345578a02a
Humaid Khan <humaidk2@gmail.com>
2020-08-08 14:26:02 +0400

    Process email sending auth based on envionment var

--- routes/signup.js
+++ routes/signup.js
@@ -1,23 +1,34 @@
 var nodemailer = require("nodemailer");
 
-var transporter = nodemailer.createTransport({
-  service: "gmail",
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: false,
-  // auth: {
-  //   user: process.env.NODE_MAIL_USER,
-  //   pass: process.env.NODE_MAIL_PASS,
-  // },
-  auth: {
-    type: "OAuth2",
-    user: process.env.MAIL_USER,
-    clientId: process.env.OAUTH_CLIENT_ID,
-    clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-    accessToken: process.env.OAUTH_ACCESS_TOKEN,
-  },
-});
+// username/pass for localhost
+// oauth2 for production
+if (process.env.TYPE === "DEVELOPMENT") {
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.NODE_MAIL_USER,
+      pass: process.env.NODE_MAIL_PASS,
+    },
+  });
+} else if (process.env.TYPE === "PRODUCTION") {
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      type: "OAuth2",
+      user: process.env.MAIL_USER,
+      clientId: process.env.OAUTH_CLIENT_ID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+      accessToken: process.env.OAUTH_ACCESS_TOKEN,
+    },
+  });
+}
 
 module.exports = function (app, passport) {
   app.post("/signup", function (req, res, next) {
@@ -32,13 +43,13 @@ module.exports = function (app, passport) {
           from: '"Money-IO" <communication.vrpacman@gmail.com>',
           to: user.email,
           subject: "Confirm registration for Money-IO",
-          text: `Hi ${user.username}!\n\nPlease verify your account by clicking the following link: ${process.env.MAIL_LINK}/verifyemail?unique=${user.token}\n\nIf you believe you have received this email in error, please ignore this email.`,
+          text: `Hi ${user.username}!\n\nPlease verify your account by clicking the following link: ${process.env.MONEY_CLIENT_URL}/verifyemail?unique=${user.token}\n\nIf you believe you have received this email in error, please ignore this email.`,
         };
 
         transporter.sendMail(mailOptions, function (error, info) {
+          console.log("email sent");
           if (error) {
             console.log(error);
-            res.status(400).send({ error: error });
           }
         });
 
